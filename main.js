34f703e2d79d2acd71790845404009fb8e97e22c
Achyuth <69086207+Arcus286@users.noreply.github.com>
2025-08-08 11:06:09 +0530

    changes the ui
    it was 15x15 so corrected and made it 16x16. Also tried adding the hover effect but need some time to figure it out.

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 let container = document.querySelector("#container");
-for(let i = 0;i<225;i++){
+for(let i = 0;i<256;i++){
     let el = document.createElement("div");
-    el.style.height = "27px";
-    el.style.width = "27px";
-    el.style.border = "2px solid black";
     el.setAttribute("class","box");
     container.append(el);
 }
 
-let box = document.querySelectorAll(".box");
+let boxes = document.querySelectorAll(".box");
+
+boxes.forEach((box)=>{
+    box.addEventListener("mouseover",(e)=>{
+        
+    })
+})
